c2e2613029195d297b66b9390102bbe0f43c0515
esen13 <esen.arykbaev@gmail.com>
2025-02-04 11:55:49 +0600

    fixed pages redirect

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,5 @@
-import { getUser } from '@/queries/user'
-import { redirect } from 'next/navigation'
 import Form from './form'
 
 export default async function Page() {
-	const user = await getUser()
-	if (user) {
-		redirect('/dashboard')
-	}
-
 	return <Form />
 }
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -45,13 +45,24 @@ export async function updateSession(request: NextRequest) {
 		!request.nextUrl.pathname.startsWith('/login') &&
 		!request.nextUrl.pathname.startsWith('/register') &&
 		!request.nextUrl.pathname.startsWith('/api') &&
-		!request.nextUrl.pathname.startsWith('/error') &&
 		!request.nextUrl.pathname.startsWith('/phone') &&
 		!request.nextUrl.pathname.startsWith('/forgot-password')
 	) {
 		// no user, potentially respond by redirecting the user to the login page
 		const url = request.nextUrl.clone()
 		url.pathname = '/login'
+		return NextResponse.redirect(url)
+	}
+
+	if (
+		user &&
+		(request.nextUrl.pathname.startsWith('/login') ||
+			request.nextUrl.pathname.startsWith('/register') ||
+			request.nextUrl.pathname.startsWith('/api') ||
+			request.nextUrl.pathname.startsWith('/phone'))
+	) {
+		const url = request.nextUrl.clone()
+		url.pathname = '/dashboard'
 		return NextResponse.redirect(url)
 	}
 
