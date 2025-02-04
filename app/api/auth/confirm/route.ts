716a332db3574b3e368f4a959ac61ffec0c53c50
esen13 <esen.arykbaev@gmail.com>
2025-02-04 09:33:49 +0600

    fixed auth

--- app/api/auth/confirm/route.ts
+++ app/api/auth/confirm/route.ts
@@ -7,16 +7,15 @@ import { redirect } from 'next/navigation'
 export async function GET(request: NextRequest) {
 	const { searchParams } = new URL(request.url)
 	const token_hash = searchParams.get('token_hash')
-	const token = searchParams.get('token')
 	const type = searchParams.get('type') as EmailOtpType | null
 	const next = searchParams.get('next') ?? '/'
 
-	if ((token || token_hash) && type) {
+	if (token_hash && type) {
 		const supabase = await createClient()
+
 		const { error } = await supabase.auth.verifyOtp({
 			type,
-			token_hash: token_hash || '',
-			token: token || ''
+			token_hash
 		})
 		if (!error) {
 			// redirect user to specified redirect URL or root of app
